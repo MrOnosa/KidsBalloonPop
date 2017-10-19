0ed672c92781317353b7c12a0948e87575fcbadf
Tyler <supersnapple@gmail.com>
2017-10-19 06:09:17 -0500

    Made a watch face circle. Tried to add an interactive square.

--- js/main.js
+++ js/main.js
@@ -24,28 +24,44 @@ resources = PIXI.loader.resources,
 Sprite = PIXI.Sprite;
 
 var stage = new Container(),
-renderer = new canvasRenderer(360,360);
+renderer = new canvasRenderer(1360,1360);
+renderer.backgroundColor = 0xFFFFFF;
 document.body.appendChild(renderer.view);
 loader
 .add([{url: "./images/cat.png"}])
 .load(setup);
 
 
-var keyObject = keyboard(asciiKeyCodeNumber);
-keyObject.press = function() {
-  //key object pressed
-};
-keyObject.release = function() {
-  //key object released
-};
-
 //Define any variables that are used in more than one function
-var cat, state;
+var cat, state, message, box;
 function setup() {
+  var circle = new PIXI.Graphics();
+  circle.lineStyle(4, 0xFF3300, 1);
+  circle.beginFill(0xF0F0F0);
+  circle.drawCircle(180, 180, 183);
+  circle.endFill();
+  stage.addChild(circle);
+  
+  box = new PIXI.Graphics();
+  box.lineStyle(4, 0xFF3300, 1);
+  box.beginFill(0xccff99);
+  box.drawRect(180, 180, 64, 64);
+  box.endFill();
+  stage.addChild(box);
+  
+  message = new PIXI.Text(
+		  "Hello Pixi!",
+		  {fontFamily: "Arial", fontSize: 32, fill: "white"}
+		);
 
+		message.x=54;
+		message.y=96;
+		stage.addChild(message);
+  
   //Create the `cat` sprite 
   cat = new Sprite(resources["./images/cat.png"].texture);
-  cat.y = 96; 
+  cat.x = 196; 
+  cat.y = 196; 
   cat.vx = 0;
   cat.vy = 0;
   stage.addChild(cat);
@@ -132,8 +148,73 @@ function play() {
   //Move the cat 1 pixel to the right each frame
   cat.x += cat.vx;
   cat.y += cat.vy;
-}
+  
+//check for a collision between the cat and the box
+  if (hitTestRectangle(cat, box)) {
 
+    //if there's a collision, change the message text
+    //and tint the box red
+    message.text = "hit!";
+    box.tint = 0xff3300;
+
+  } else {
+
+    //if there's no collision, reset the message
+    //text and the box's color
+    message.text = "No collision...";
+    box.tint = 0xccff99;
+  }
+}
+function hitTestRectangle(r1, r2) {
+
+	  //Define the variables we'll need to calculate
+	  var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+	  //hit will determine whether there's a collision
+	  hit = false;
+
+	  //Find the center points of each sprite
+	  r1.centerX = r1.x + r1.width / 2;
+	  r1.centerY = r1.y + r1.height / 2;
+	  r2.centerX = r2.x + r2.width / 2;
+	  r2.centerY = r2.y + r2.height / 2;
+
+	  //Find the half-widths and half-heights of each sprite
+	  r1.halfWidth = r1.width / 2;
+	  r1.halfHeight = r1.height / 2;
+	  r2.halfWidth = r2.width / 2;
+	  r2.halfHeight = r2.height / 2;
+
+	  //Calculate the distance vector between the sprites
+	  vx = r1.centerX - r2.centerX;
+	  vy = r1.centerY - r2.centerY;
+
+	  //Figure out the combined half-widths and half-heights
+	  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+	  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+	  //Check for a collision on the x axis
+	  if (Math.abs(vx) < combinedHalfWidths) {
+
+	    //A collision might be occuring. Check for a collision on the y axis
+	    if (Math.abs(vy) < combinedHalfHeights) {
+
+	      //There's definitely a collision happening
+	      hit = true;
+	    } else {
+
+	      //There's no collision on the y axis
+	      hit = false;
+	    }
+	  } else {
+
+	    //There's no collision on the x axis
+	    hit = false;
+	  }
+
+	  //`hit` will be either `true` or `false`
+	  return hit;
+	};
 function keyboard(keyCode) {
   var key = {};
   key.code = keyCode;
