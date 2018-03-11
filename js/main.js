9f6552c11a439166e5b7b391a3958f1c9019c3a9
Tyler <supersnapple@gmail.com>
2018-03-11 15:26:45 -0500

    Correcting canvas size. Using Ticker for rendering updates. Fixing CSS.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 
 window.onload = function () {
-    // TODO:: Do your initialization job
-
     // add eventListener for tizenhwkey
     document.addEventListener('tizenhwkey', function(e) {
         if(e.keyName === "back") {
@@ -9,66 +7,50 @@ window.onload = function () {
 			    tizen.application.getCurrentApplication().exit();
 			} catch (ignore) {
 			}
-
-
 		}
 	});    
 };
 
-//Aliases
-var Container = PIXI.Container,
-autoDetectRenderer = PIXI.autoDetectRenderer,
-canvasRenderer = PIXI.CanvasRenderer,
-loader = PIXI.loader,
-resources = PIXI.loader.resources,
-Sprite = PIXI.Sprite;
-
-var stage = new Container(),
-renderer = new canvasRenderer(1360,1360);
-renderer.backgroundColor = 0xFFFFFF;
-document.body.appendChild(renderer.view);
-loader
-.add([{url: "./images/frame_0_delay-0.1s.png"}])
-.load(setup);
+var app = new PIXI.Application(360, 360, {backgroundColor : 0xF0F0F0, forceCanvas: true});
+document.body.appendChild(app.view);
+PIXI.loader.add([{url: "./images/frame_0_delay-0.1s.png"}])
+      .load(setup);
 
 
 //Define any variables that are used in more than one function
-var state, drawables;
+var  drawables = [];
 var balloonHeight = 141;
 var balloonWidth = 123;
 var balloonFrame = new PIXI.Rectangle(84, 62, balloonWidth, balloonHeight);
-drawables = [];
+
 function setup() {
   var circle = new PIXI.Graphics();
   circle.lineStyle(4, 0xFF3300, 1);
-  circle.beginFill(0xF0F0F0);
+  circle.beginFill(0xFFFFFF);
   circle.drawCircle(180, 180, 183);
   circle.endFill();
-  stage.addChild(circle);
+  //app.stage.addChild(circle);
       
   var texture = PIXI.TextureCache["./images/frame_0_delay-0.1s.png"];
   texture.frame = balloonFrame;
   for(i = 0; i < 4; i++)
   {
-    var sprite = new Sprite(texture);
+    var sprite = new PIXI.Sprite(texture);
     sprite.x = Math.random()*360 - (balloonWidth/2); 
     sprite.y = Math.random()*365; 
     sprite.vx = 0;
-    sprite.vy = -10;
+    sprite.vy = -1;
     sprite.interactive = true;
     sprite.on('pointerdown', onClick);
     drawables.push(sprite);
   }
 
   drawables.forEach(function(drawable){
-    stage.addChild(drawable);
+    app.stage.addChild(drawable);
   });
 
-  //Set the game state
-  state = play;
-
   //Start the game loop
-  gameLoop();
+  app.ticker.add(play);
 }
 
 function onClick (s) {
@@ -78,22 +60,10 @@ function onClick (s) {
   var i = drawables.indexOf(s.currentTarget);
 }
 
-function gameLoop(){
-
-  //Loop this function 60 times per second
-  requestAnimationFrame(gameLoop);
-
-  //Update the current game state:
-  state();
-
-  //Render the stage
-  renderer.render(stage);
-}
-
-function play() {
+function play(delta) {
   drawables.forEach(function(drawable){
-    drawable.x += drawable.vx;
-    drawable.y += drawable.vy;
+    drawable.x += delta * drawable.vx;
+    drawable.y += delta * drawable.vy;
 
     if(drawable.y < -balloonHeight)
     {
